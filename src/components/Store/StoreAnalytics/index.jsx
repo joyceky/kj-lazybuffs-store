6e86c717d1ebf6d712c15ba05c474cfcb933f3eb
Kyle Joyce <kyyjoyce@gmail.com>
2017-03-08 12:05:29 -0700

    update with lazybuffs styling

--- src/components/Store/StoreAnalytics/index.jsx
+++ src/components/Store/StoreAnalytics/index.jsx
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getStoreCompletedOrders } from '../../../actions';
 import ListItemCompleted from './ListItemCompleted';
-import BarChartComponent from './Graphs/BarChart';
+import BarChartComponent from './Analytics//BarChart';
 import axios from 'axios';
 import { API_URL } from '../../../actions';
 
 class StoreAnalytics extends Component {
   constructor(){
     super();
+
     const today = new Date();
 
     this.state = {
@@ -30,11 +31,6 @@ class StoreAnalytics extends Component {
   getOrderData(month, year) {
     axios.post(`${API_URL}/store/orders/analytics`, { auth: this.props.auth, month: month, year: year })
       .then(({ data }) => {
-        console.log("Url ", `${API_URL}/store/orders/analytics`);
-        console.log("Auth ", this.props.auth);
-        console.log("Month ", month, "Year ", year);
-        console.log("Data ", data);
-
         this.setState({ month, year, orders: data});
       })
       .catch((err) => {
@@ -43,18 +39,17 @@ class StoreAnalytics extends Component {
   }
 
   selectMonth(e){
-    // this.setState({ month: parseInt(e.target.value) });
-    console.log("etarget", e.target.value);
+
     this.getOrderData(e.target.value, this.state.year)
   }
 
   selectYear(e){
-    // this.setState({ year: parseInt(e.target.value) });
     this.getOrderData(this.state.month, e.target.value);
   }
 
   formatData(orders) {
-    const daysNum = new Date(this.state.year, this.state.month, 0).getDate();
+    let month = parseInt(this.state.month)+1;
+    const daysNum = new Date(this.state.year, month, 0).getDate();
     const days = [];
 
     for (var i = 1; i <= daysNum; i++) { days.push(i) };
@@ -86,41 +81,58 @@ class StoreAnalytics extends Component {
     return (
       <div>
       <section style={style.container}>
-        <section style={style.header}>
-          <section>
-            <span>Select a month to view analytics for {`${this.props.auth.storeName}: `}</span>
-            <select style={style.select} onChange={this.selectMonth} value={this.state.month}>
-                {months.map((monthVal, i) => {
-              return <option value={i}>{monthVal}</option>
-            })}
-            </select>
-            <select style={style.select} onChange={this.selectYear} value={this.state.year}>
-            {
-              [2014,2015,2016,2017]
-              .map((year) => {
-                return <option value={year}>{year}</option>
-              })
-            }
-            </select>
-          </section>
-        </section>
 
+           <section style={style.header}>
+             <section style={style.buttonContainer}>
+               <section  style={style.buttonContainerDropdowns}>
 
-          <section style={style.chartContainer}>
-          {this.state.orders.length > 0
+                 <select style={style.select} onChange={this.selectMonth} value={this.state.month}>
+                   {
+                      months.map((monthVal, i) => {
+                        return <option value={i} key={i}>{monthVal}</option>
+                      })
+                    }
+                  </select>
+
+                  <select style={style.select} onChange={this.selectYear} value={this.state.year}>
+                    {
+                      [2014,2015,2016,2017]
+                      .map((year, i) => {
+                        return <option key={i} value={year}>{year}</option>
+                      })
+                    }
+                  </select>
+                </section>
+
+                  <button className='generate-invoice-button' style={style.btn} onClick={() => this.toggleInvoice()}>Generate Invoice</button>
+              </section>
+
+              <section>
+                <p>{`Orders: ${ this.state.orders.length }`}</p>
+                <p>{`Revenue: $${ (this.calcRevenue(this.state.orders)) }`}</p>
+              </section>
+            </section>
+
+          { this.state.loading ?
+              <span style={style.subContainer}>
+                <h1 style={style.title}>Loading analytics...</h1>
+              </span>
+              : null }
+
+          { !this.state.loading && this.state.orders.length > 0
           ?
-          <div style={{zIndex: '0'}}>
-            <p>Orders Analytics</p>
-            <BarChartComponent orders={this.formatData(this.state.orders)} dataKey="orders" color="#7830ee" />
+          <div>
+            <section style={style.chartContainer}>
+              <div style={{zIndex: '0'}}>
+                <p>Orders Analytics</p>
+                <BarChartComponent orders={this.formatData(this.state.orders)} dataKey="orders" color="#CFB87C" />
 
-            <p>Revenue Analytics</p>
-            <BarChartComponent orders={this.formatData(this.state.orders)} dataKey="total" color="#29cb56" />
-
-            <p>{`${this.props.auth.storeName} Revenue for ${months[this.state.month]} ${this.state.year}: $${ this.calcRevenue(this.state.orders)}`}</p>
+                <p>Revenue Analytics</p>
+                <BarChartComponent orders={this.formatData(this.state.orders)} dataKey="total" color="#A2A4A3" />
+              </div>
+            </section>
           </div>
-
           : <h1 style={style.noOrdersTitle}>No Orders for {`${months[this.state.month]} ${this.state.year}`}</h1>}
-        </section>
 
         <ul style={style.listStyle}>
           {this.state.orders.map((order) => {
@@ -138,23 +150,57 @@ const style = {
     marginTop: '50px',
     display: 'flex',
     flexDirection: 'column',
+    width: '100%'
+  },
+  subContainer: {
+    display: 'flex',
     width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  btn: {
+    borderRadius: '5px',
+    display: 'block',
+    padding: '8px 10px',
+    fontSize: '22px',
+    textDecoration: 'none',
+    color: '#fff',
+    backgroundColor: 'black',
+    boxShadow: '0px 5px 0px 0px #565A5C',
+    fontSize: '18px'
+  },
+  title: {
+    fontSize: '20px',
+    margin: '16px',
+    padding: '0',
   },
   select: {
-    height: '22px',
-    fontSize: '16px',
-    margin: '8px'
+    height: '35px',
+    fontSize: '18px',
+    margin: '8px',
+    backgroundColor: '#fff',
+    color: "#565A5C"
   },
   header: {
-    padding: '16px',
+    padding: '64px',
     width: '100%',
     boxSizing: 'border-box',
     display: 'flex',
+    flexDirection: 'column',
+    fontSize: '18px',
+  },
+  buttonContainer: {
+    display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'center',
-    fontSize: '20px'
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttonContainerDropdown: {
+
   },
   chartContainer: {
+    maxWidth: '100%',
+    overflow: 'scroll',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
@@ -162,37 +208,6 @@ const style = {
     width: 'auto',
     fontSize: '20px',
     textAlign: 'center'
-  },
-  listStyle: {
-    listStyle: 'none',
-    margin: '0',
-    width: '100%',
-    padding: '0',
-  },
-  listSortBy: {
-    display: 'flex',
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    margin: '8px',
-  },
-  listSortByButton: {},
-  allOrdersTitle: {
-    margin: '0',
-    padding: '16px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderBottom: '2px solid lightgrey',
-  },
-  noOrdersTitle: {
-    fontSize: '20px',
-    margin: '0',
-    padding: '16px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   previewBanner: {
     top: '50px',
@@ -203,8 +218,8 @@ const style = {
     backgroundColor: '#414141',
     textAlign: 'center',
     color: '#FFB300',
-  },
-};
+  }
+}
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ getStoreCompletedOrders }, dispatch);
